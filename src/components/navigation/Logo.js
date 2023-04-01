046b04aa151da20cbe51e2d9e3a083f4dc9ba11c
Job Kroon <jobkroon@me.com>
2023-04-01 15:56:15 +0200

    Code review

--- src/components/navigation/Logo.js
+++ src/components/navigation/Logo.js
@@ -5,7 +5,7 @@ import brain from './brain.png';
 const Logo = () => {
    return (
       <Tilt>
-         <div className="pa3 ma6 mt0 br3 dim shadow-4 link pointer " style={{ height: '150px', width:'150px', backgroundColor: 'white' }}>
+         <div className="pa3 ma6 mt0 br3 dim shadow-4 link pointer " style={{ height: '150', width:'150', backgroundColor: 'white' }}>
             <img style={{ paddingTop: '8px' }} src={brain} alt='Brain Logo'></img>
          </div>
       </Tilt>
